78250460ae2cb34a79d9cee2c942b1c9ea78ddf0
dilini <dilinichamikasilva@gmail.com>
2025-05-08 12:19:47 +0530

    added some animations

---
+++ portfolio.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    const phrases = ["Web Developer", "Web Designer", "Freelancer"];
+    const $el = $(".highlight");
+    let currentPhrase = 0;
+    let currentChar = 0;
+    let isDeleting = false;
+
+    function type() {
+        const text = phrases[currentPhrase];
+
+        if (isDeleting) {
+            currentChar--;
+        } else {
+            currentChar++;
+        }
+
+        $el.text(text.substring(0, currentChar));
+        let speed = isDeleting ? 50 : 100;
+
+        if (!isDeleting && currentChar === text.length) {
+            speed = 1500;
+            isDeleting = true;
+        }
+
+        else if (isDeleting && currentChar === 0) {
+            isDeleting = false;
+            currentPhrase = (currentPhrase + 1) % phrases.length;
+            speed = 500;
+        }
+
+        setTimeout(type, speed);
+    }
+
+    type();
+});
